ead33b294810e011c928c8df9520f63db2e8cc25
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-05-10 10:21:09 -0700

    seperated responsibility of painting  dark button and paiting a light button out of draw_button() and into two new methods, draw_light_button, and draw_dark_button

--- main.rb
+++ main.rb
@@ -1,25 +1,22 @@
+def draw_light_button(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
+end
+
+def draw_dark_button(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
+end
+
 
 class button
 
-  attr_reader :label_text, :x, :y, :foreground_color, :is_dark_mode
+  attr_reader :label_text, :x, :y, :foreground_color
 
-  def initialize(label_text, x, y, foreground_color, is_dark_mode)
+  def initialize(label_text, x, y, foreground_color)
     @label_text = label_text
     @x = x
     @y = y
     @foreground_color = foreground_color
     @is_dark_mode = is_dark_mode
   end
-
-  def draw_button(button)
-    if button.is_dark_mode
-      # darken foreground color for dark mode
-      paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
-    else
-      # lighten foreground color for non-dark mode
-      paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
-    end
-  end
-  
 
 end
